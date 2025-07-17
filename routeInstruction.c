e2f488019bc848482b281090859f26b002e5c7a6
grash2024 <grash2024@gmail.com>
2025-07-17 11:53:11 +0530

    added routeInstruction

---
+++ routeInstruction.c
@@ -0,0 +1,22 @@
+
+tabs 
+
+home page -> /app/(drawer)/(tabs)/home.tsx
+myGroups page -> /app/(drawer)/(tabs)/home.tsx
+profile page -> /app/(drawer)/(tabs)/profile.tsx
+
+grouping 1 (payments)
+
+payments -> /app/(drawer)/(tabs)/(payments)/index
+view press -> /app/(drawer)/(tabs)/(payments)/paymentsGroupInfo
+view more (on right side) -> /app/(drawer)/(tabs)/(payments)/filteredPayments.tsx
+
+
+grouping 2 home on singular button press -> (groups)
+there are two singular button routing to  gold saving chit scheme , fixed chit scheme both routes to saving.tsx 
+
+on pressing know more -> /app/(drawer)/(tabs)/(groups)/knowMore/[id]/index.tsx
+on pressing join noe -> app/(drawer)/(tabs)/(groups)/enrollForm.tsx  (not yet opening)
+ 
+
+on pressing more drawer will bw opened
